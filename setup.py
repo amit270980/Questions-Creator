434c461b045ee1bd93b92dd897c08af443d6ee66
amit270980 <amit270980@gmail.com>
2024-06-13 22:14:48 +1000

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.0',
+    author="Amit Kumar",
+    author_email="amit270980@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
